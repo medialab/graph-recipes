b3fe045ed11677d8d2c2148c37fddd9bb1edb153
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2016-08-25 11:07:49 +0200

    add minimal info

--- app/view_board/board.js
+++ app/view_board/board.js
@@ -12,21 +12,36 @@ angular.module('gsurgery.view_board', ['ngRoute'])
 .controller('BoardCtrl', ['$scope', '$timeout',  'store', '$location'
   ,function(               $scope ,  $timeout ,   store ,  $location) {
   
-  // Init
-  window.g = store.get('graph')
-  if (g) {
-    json_graph_api.buildIndexes(g)
-  } else {
-    $timeout(function(){
-      $location.url('/upload')
-    }, 0)
+  // Scope variables
+  $scope.filename
+  $scope.originalGraph
+  $scope.nodesCount
+  $scope.edgesCount
+
+  // Scope functions
+  $scope.refreshGraph = function () {
+    window.g = $scope.originalGraph
+    if (window.g) {
+      json_graph_api.buildIndexes(g)
+      $scope.nodesCount = g.nodes.length
+      $scope.edgesCount = g.edges.length
+    } else {
+      $timeout(function(){
+        $location.url('/upload')
+      }, 0)
+    }
   }
 
   $scope.downloadOutput = function () {
-
     var blob = new Blob(json_graph_api.buildGEXF(window.g), {'type':'text/gexf+xml;charset=utf-8'});
     saveAs(blob, store.get('graphname') + " (edited).gexf");
-
   }
+
+  // Init
+  $scope.filename = store.get('graphname')
+  $scope.originalGraph = store.get('graph')
+  $scope.refreshGraph()
+
+  // Processing
 
 }]);
