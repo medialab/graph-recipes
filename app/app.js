af297cd5f9e2a19408aa6b7f108266d1984e4965
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-02-06 16:03:23 +0100

    lcd timer system

--- app/app.js
+++ app/app.js
@@ -24,12 +24,11 @@ config(['$routeProvider', function($routeProvider) {
 })
 
 // Directives
-.directive('lcd', function ($timeout) {
+.directive('lcd', function ($timeout, $interval) {
   return {
     restrict: 'A',
     scope: {
-      text: '=',
-      direction: '='
+      animStatus: '='
     },
     templateUrl: 'lcd.html',
     link: function(scope, element, attrs, ctrl) {
@@ -81,23 +80,78 @@ config(['$routeProvider', function($routeProvider) {
         "thinkbubble",
         "background"
       ]
+      var animIntervals = []
 
       // obj.removeClass("ng-hide");
 
       $timeout(function(){
         sprites.forEach(hideSprite)
-        showSprite('background')
-        showSprite('closeddoor')
-        showSprite('closedovendoor')
-        showSprite('clienthead3')
-        showSprite('cooksleeping')
+        scope.$watch('animStatus', applyStatus)
       }, 100)
       
       function hideSprite(s) {
         obj[0].contentDocument.getElementById(s).style.display = "none"
       }
+      
       function showSprite(s) {
         obj[0].contentDocument.getElementById(s).style.display = "inline"
+      }
+
+      function applyStatus(e, os, ns) {
+        if (os != ns) {
+          animIntervals.forEach(function(interval){
+            interval.cancel()
+          })
+          switch (scope.animStatus) {
+            case 'off':
+              sprites.forEach(hideSprite)
+              break;
+
+            case 'choose-recipe':
+              sprites.forEach(hideSprite)
+              showSprite('background')
+              showSprite('closeddoor')
+              showSprite('closedovendoor')
+              showSprite('cooksleeping')
+              addAnimInterval([
+                {'zzz': true}, {'zzz': false}
+              ], 600)
+              addAnimInterval([
+                {'clienthead3': true , 'thinkbubble': true , 'clienthead1': false, 'clientarm1': false, 'menu': false},
+                {'clienthead3': false, 'thinkbubble': false, 'clienthead1': true , 'clientarm1': true , 'menu': true}
+              ], 2000)
+              break;
+
+            default:
+              sprites.forEach(hideSprite)
+              break;
+          }
+        }
+      }
+
+      function addAnimInterval(sequence, delay) {
+        var sprite
+        for(sprite in sequence[0]) {
+          if (sequence[0][sprite]) {
+            showSprite(sprite)
+          } else {
+            hideSprite(sprite)
+          }
+        }
+
+        var count = 1
+        var interval = $interval(function(){
+          count = (count + 1)%sequence.length
+          
+          for(sprite in sequence[count]) {
+            if (sequence[count][sprite]) {
+              showSprite(sprite)
+            } else {
+              hideSprite(sprite)
+            }
+          }
+        }, delay || 500)
+        animIntervals.push(interval)
       }
     }
   }
