1c8ec0bdf094af4d862bfd44e887c795e25a0268
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-02-06 13:27:11 +0100

    integrate lcd

--- app/app.js
+++ app/app.js
@@ -22,3 +22,74 @@ config(['$routeProvider', function($routeProvider) {
     return Math.round(+d*100)+'%'
   }
 })
+
+// Directives
+.directive('lcd', function ($timeout) {
+  return {
+    restrict: 'A',
+    scope: {
+      text: '=',
+      direction: '='
+    },
+    templateUrl: 'lcd.html',
+    link: function(scope, element, attrs, ctrl) {
+      var obj = element.find('object')
+      var sprites = [
+        "wait",
+        "smokesmall",
+        "smokebig",
+        "splash",
+        "casserole",
+        "flyingfoodbit",
+        "cookoven",
+        "cooktop1",
+        "cooktop2",
+        "cooklegs",
+        "cookarmknife1",
+        "cookarmknife2",
+        "cookarmknife3",
+        "foodcut",
+        "foodbits",
+        "foodstackbottom",
+        "foodstackmiddle",
+        "foodstacktop",
+        "cooksleeping",
+        "zzz",
+        "ovenfood",
+        "closedovendoor",
+        "openovendoor",
+        "closeddoor",
+        "opendoor",
+        "cookstanding",
+        "cookarmnote",
+        "pen3",
+        "pen2",
+        "pen1",
+        "cookserving",
+        "cookbubble",
+        "clientbubble",
+        "menu",
+        "spoon",
+        "dish",
+        "clientarm1",
+        "clientarm2",
+        "flyingdishpiece1",
+        "flyingdishpiece2",
+        "clienthead1",
+        "clienthead2",
+        "clienthead3",
+        "thinkbubble",
+        "background"
+      ]
+
+      // obj.removeClass("ng-hide");
+
+      $timeout(function(){
+        sprites.forEach(function(sprite) {
+          obj[0].contentDocument.getElementById(sprite).style.display = "none"
+        })
+      }, 100)
+      
+    }
+  }
+})
