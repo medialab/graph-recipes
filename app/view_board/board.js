13e6e963d6d2dfb530697554a601725bab4c7326
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-02-07 11:27:10 +0100

    Integrate js editor

--- app/view_board/board.js
+++ app/view_board/board.js
@@ -9,8 +9,8 @@ angular.module('graphrecipes.view_board', ['ngRoute'])
   });
 }])
 
-.controller('BoardCtrl', ['$scope', '$timeout',  'store', '$location', 'recipesList'
-  ,function(               $scope ,  $timeout ,   store ,  $location,   recipesList ) {
+.controller('BoardCtrl', ['$scope', '$timeout',  'store', '$location', 'recipesList', '$http'
+  ,function(               $scope ,  $timeout ,   store ,  $location,   recipesList ,  $http) {
   
   // Scope variables
   $scope.filename
@@ -40,6 +40,20 @@ angular.module('graphrecipes.view_board', ['ngRoute'])
 
   $scope.pickRecipe = function(r) {
     $scope.recipe = r
+    $http.get('recipes/'+r.file).then(function (data) {
+      $timeout(function(){
+        // INITIALIZATION
+        if(data.data){
+          document.querySelector('#js-editor').textContent = data.data
+        }
+        // Init Ace JS editor panel
+        // Note: we keep editor in global scope to be able to edit settings from the console
+        window.editor = ace.edit("js-editor");
+        window.editor.setTheme("ace/theme/chrome");
+        window.editor.setFontSize(14)
+        window.editor.getSession().setMode("ace/mode/javascript");
+      })
+    })
   }
 
   $scope.closeRecipe = function() {
