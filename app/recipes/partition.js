f20e3a15737cc3aafcd7d1f78a570a918c5075a7
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2017-02-21 09:08:52 +0100

    draw partition matrix

--- app/recipes/partition.js
+++ app/recipes/partition.js
@@ -62,6 +62,7 @@ g.edges().forEach(function(eid){
 
 console.log('Attribute data', attData)
 
+drawFlowMatrix(attData)
 
 // ---
 // Functions
@@ -79,4 +80,133 @@ function getType(str){
   }
   else if (isfloat.test(str) || commaFloat.test(str) || dotFloat.test(str)) return "float";
   else return "string";
+}
+
+function drawFlowMatrix(attData) {
+	// Compute crossings
+	var crossings = []
+	var v1
+	var v2
+	for (v1 in attData.valueFlow) {
+		for (v2 in attData.valueFlow[v1]) {
+			crossings.push({
+				v1: v1,
+				v2: v2,
+				count: attData.valueFlow[v1][v2].count
+			})
+		}
+	}
+
+	// Rank values by count
+	var sortedValues = attData.values.sort(function(v1, v2){
+		return attData.valuesIndex[v2] - attData.valuesIndex[v1]
+	})
+	var valueRanking = {}
+	sortedValues.forEach(function(v, i){
+		valueRanking[v] = i
+	})
+
+	// Draw SVG
+	var maxR = 24
+	var margin = {top: 150, right: 24, bottom: 64, left: 150}
+	var width = 2 * maxR * attData.values.length
+	var height = width // square space
+
+	var x = d3.scaleLinear()
+	  .range([0, width]);
+
+	var y = d3.scaleLinear()
+	  .range([0, height]);
+
+	var size = d3.scaleLinear()
+	  .range([0, 0.95 * maxR])
+
+	var a = function(r){
+	  return Math.PI * Math.pow(r, 2)
+	}
+
+	var r = function(a){
+	  return Math.sqrt(a/Math.PI)
+	}
+
+	x.domain([0, attData.values.length - 1])
+	y.domain([0, attData.values.length - 1])
+	size.domain(d3.extent(crossings, function(d){return r(d.count)}))
+
+	var svg = d3.select('#playground').append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	// Horizontal lines
+	svg.selectAll('line.h')
+	    .data(attData.values)
+	  .enter().append('line')
+	    .attr('class', 'h')
+	    .attr('x1', 0)
+	    .attr('y1', function(d){ return y(valueRanking[d]) })
+	    .attr('x2', width)
+	    .attr('y2', function(d){ return y(valueRanking[d]) })
+	    .style("stroke", 'rgba(0, 0, 0, 0.06)')
+
+	// Vertical lines
+	svg.selectAll('line.v')
+	    .data(attData.values)
+	  .enter().append('line')
+	    .attr('class', 'v')
+	    .attr('x1', function(d){ return x(valueRanking[d]) })
+	    .attr('y1', 0)
+	    .attr('x2', function(d){ return x(valueRanking[d]) })
+	    .attr('y2', height)
+	    .style("stroke", 'rgba(0, 0, 0, 0.06)')
+
+	// Horizontal labels
+	svg.selectAll('text.h')
+	    .data(attData.values)
+	  .enter().append('text')
+	    .attr('class', 'h')
+	    .attr('x', -6-maxR)
+	    .attr('y', function(d){ return y(valueRanking[d]) + 3 })
+	    .text( function (d) { return d })
+	    .style('text-anchor', 'end')
+	    .attr("font-family", "sans-serif")
+	    .attr("font-size", "12px")
+	    .attr("fill", 'rgba(0, 0, 0, 0.5)')
+
+	// Vertical labels
+	svg.selectAll('text.v')
+	    .data(attData.values)
+	  .enter().append('text')
+	    .attr('class', 'v')
+	    .attr('x', function(d){ return x(valueRanking[d]) + 3 })
+	    .attr('y', -6-maxR)
+	    .text( function (d) { return d })
+	    .style('text-anchor', 'end')
+	    .style('writing-mode', 'vertical-lr')
+	    .attr("font-family", "sans-serif")
+	    .attr("font-size", "12px")
+	    .attr("fill", 'rgba(0, 0, 0, 0.5)')
+
+	// Dots
+	var dot = svg.selectAll(".dot")
+	    .data(crossings)
+	  .enter().append('g')
+	  
+	dot.append("circle")
+	  .attr("class", "dot")
+	  .attr("r", function(d) { return size(r(d.count) ) })
+	  .attr("cx", function(d) { return x(valueRanking[d.v1]) })
+	  .attr("cy", function(d) { return y(valueRanking[d.v2]) })
+	  .style("fill", 'rgba(120, 120, 120, 0.3)')
+
+ dot.append('text')
+	    .attr('x', function(d){ return x(valueRanking[d.v1]) })
+	    .attr('y', function(d){ return y(valueRanking[d.v2]) + 4 })
+	    .text( function (d) { return d.count })
+	    .style('text-anchor', 'middle')
+	    .attr("font-family", "sans-serif")
+	    .attr("font-size", "10px")
+	    .attr("fill", 'rgba(0, 0, 0, 1.0)')
+
 }
